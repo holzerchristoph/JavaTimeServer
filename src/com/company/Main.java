578c7e7fb0d118df4bd98c65dd2c4349b6efb35b
holzerchristoph <holzer.christoph@rolmail.net>
2018-09-24 17:47:01 +0200

    sent current time every 2 seconds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,28 @@ package com.company;
 
 import java.io.IOException;
 import java.net.*;
+import java.time.LocalDateTime;
 
 public class Main {
 
-    static int PORT = 5555;
+    static int DESTINATION_PORT = 9999;
+    static int SOURCE_PORT = 5555;
     static InetAddress broadcast;
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
         // define broadcast address 192.168.1.255/24
-        broadcast = InetAddress.getByName( "192.168.1.255" );
+        broadcast = InetAddress.getByName( "192.168.1.255"  );  // or 255.255.255.255
 	    // open a serversocket UDP
-        DatagramSocket serverSocket = new DatagramSocket(5555);
+        DatagramSocket serverSocket = new DatagramSocket(Main.SOURCE_PORT);
 
         while(true){
+            String time=String.valueOf(LocalDateTime.now());
+            System.out.println(time);
             // create payload
-            byte[] payload="hallo".getBytes();
+            byte[] payload=time.getBytes();
             // define the packet to send
-            DatagramPacket sendPacket = new DatagramPacket(payload, payload.length, Main.broadcast, Main.PORT);
+            DatagramPacket sendPacket = new DatagramPacket(payload, payload.length, Main.broadcast, Main.DESTINATION_PORT);
             // send packet via serversocket
             serverSocket.send(sendPacket);
             // wait
